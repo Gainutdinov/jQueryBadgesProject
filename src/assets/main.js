82cd8dbf94cb80892238d902357a9c9118b33588
Gaynetdin <m.gaynutdinov@mail.ru>
2018-05-22 00:46:28 +0400

    badges are displayed by using jQuery ajax call.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,12 @@
-$(function() {
-
-  // your code will go here
-
+$(function () {
+  $.ajax({
+    url: 'https://www.codeschool.com/users/MaratGainutdinov.json',
+    dataType: 'jsonp',
+    success: function (response) {
+      $.each(response.courses.completed, function (index, course) {
+        $("#badges").append('<div class="course">' + '<h3>' + course.title + '</h3>' +
+          '<img src="' + course.badge + '">' + '<a class="btn btn-primary" target="_blank" href="' + course.url + '">See Course</a>' + '</div>');
+      });
+    }
+  });
 });
